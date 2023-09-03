75cc47366363735926dd9e14dbe1a1bf8b8e0011
Vitalii-Dashivets <122096100+Vitalii-Dashivets@users.noreply.github.com>
2023-09-03 20:31:32 +0300

    Add posts screen

---
+++ src/Screens/PostsScreen/User.js
@@ -0,0 +1,79 @@
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+
+import GridIcon from "../../Img/grid.svg";
+import UserIcon from "../../Img/user.svg";
+import Union from "../../Img/Union2.svg";
+export const User = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Image
+          source={{ uri: "https://reactjs.org/logo-og.png" }}
+          style={styles.image}
+        ></Image>
+        <View style={styles.textBox}>
+          <Text style={styles.name}>Name</Text>
+          <Text style={styles.email}>Email</Text>
+        </View>
+      </View>
+      {/* <View style={styles.box}>
+        <Pressable style={styles.icon}>
+          <GridIcon width={24} height={24} />
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Union width={13} height={13} />
+        </Pressable>
+        <Pressable style={styles.icon}>
+          <UserIcon width={24} height={24} />
+        </Pressable>
+      </View> */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    // height: "100%",
+    backgroundColor: "red",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+
+    borderColor: "rgba(0,0,0,0.3)",
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingTop: 32,
+    paddingBottom: 32,
+  },
+  box: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    backgroundColor: "white",
+    width: 171,
+    height: 60,
+    marginTop: 9,
+    alignItems: "center",
+  },
+  textBox: {
+    marginLeft: 8,
+  },
+  name: {
+    fontSize: 13,
+    // fontWeight: 700,
+    lineHeight: 15,
+  },
+  email: {
+    fontSize: 11,
+    // fontWeight: 400,
+    lineHeight: 13,
+  },
+  image: {
+    width: 60,
+    height: 60,
+
+    borderRadius: 16,
+  },
+});
